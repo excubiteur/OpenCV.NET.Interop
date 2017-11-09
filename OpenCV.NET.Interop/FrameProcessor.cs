0b4cbdbd40c9471b2ef9e32a5212d37c4a654d41
excubiteur <excubiteur@hotmail.com>
2017-11-09 21:51:36 +0800

    First commit

---
+++ OpenCV.NET.Interop/FrameProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCV.NET.Interop
+{
+    public delegate Int32 CallbackType([MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)]byte[] image_bytes, Int32 size, Int32 height, Int32 width);
+
+    public class FrameProcessor
+    {
+        [DllImport("OpenCVFrameProcessor.dll")]
+        public static extern Int16 ProcessFrame(byte[] bytes, Int32 height, Int32 width);
+
+        [DllImport("OpenCVFrameProcessor.dll")]
+        public static extern void SetCallback(IntPtr callback);
+
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Test
+{
+    class Program
+    {
+        static Int32 Callback(byte[] image_bytes, int size, int height, int width)
+        {
+            var filename = Environment.CurrentDirectory + "\\resized.bmp";
+
+            var bitmap = new Bitmap(width,
+                height,
+                width * 4, PixelFormat.Format32bppRgb,
+                Marshal.UnsafeAddrOfPinnedArrayElement(image_bytes, 0));
+            bitmap.Save(filename);
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            OpenCV.NET.Interop.CallbackType c = new OpenCV.NET.Interop.CallbackType(Callback);
+            OpenCV.NET.Interop.FrameProcessor.SetCallback(Marshal.GetFunctionPointerForDelegate(c));
+
+            var filename = Environment.CurrentDirectory + "\\bitmap.bmp";
+            if (!File.Exists(filename))
+                return;
+
+            byte[] bytes;
+            using (var bitmap = new Bitmap(filename))
+            {
+                var rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+                var data = bitmap.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format32bppRgb);
+                IntPtr ptr = data.Scan0;
+                int numBytes = data.Stride * bitmap.Height;
+                bytes = new byte[numBytes];
+                System.Runtime.InteropServices.Marshal.Copy(ptr, bytes, 0, numBytes);
+                bitmap.UnlockBits(data);
+                OpenCV.NET.Interop.FrameProcessor.ProcessFrame(bytes,bitmap.Height, bitmap.Width);
+            }
+
+        }
+    }
+}
